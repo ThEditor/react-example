7db93ecc6a22076d6ff465b73772c5d1d3400c52
ThEditor <theditormcpe@gmail.com>
2024-04-13 20:29:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  const name = "John Doe";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, {name}!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
